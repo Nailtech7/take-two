14af3e8c832cd6c1d0cff72a09a93a3b02c81a58
Nailtech <himajacheemala7@gmail.com>
2025-04-23 16:40:32 +0530

    implement data fetching from api and storing in appwrite db

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ function TabIcon({ focused, icon, title }: any) {
 		return (
 			<ImageBackground
 				source={images.highlight}
-				className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'
+				className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
 			>
 				<Image
 					source={icon}
